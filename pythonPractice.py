9c6db4dd1fb8b8eff9189de47cd5a2c70e146130
saikrishnaboddu <39978856+saikrishnaboddu@users.noreply.github.com>
2023-03-27 16:59:13 -0400

    pythonPracticce

---
+++ pythonPractice.py
@@ -0,0 +1,6 @@
+# Day 1 - print , input and variables
+
+greeting = print("Hello " + input("Enter your name: ") + " !")
+print( len( input("Enter your name: ")))
+
+#Use Thonny - https://thonny.org/ to debug (pref a line of code)
